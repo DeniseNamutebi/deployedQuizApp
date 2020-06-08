42538149d076c73158eba78f78b3d409bb444401
Denise Namutebi <you@example.com>
2020-06-08 10:23:42 +0100

    Updates

---
+++ precache-manifest.1076d602dc5861febcbc6fa37a9ad503.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bea74d6176f1b76a17594a85da91a959",
+    "url": "/deployedQuizApp/index.html"
+  },
+  {
+    "revision": "ebd628652e1d740dd02b",
+    "url": "/deployedQuizApp/static/css/main.7c3d2140.chunk.css"
+  },
+  {
+    "revision": "9a869a154d352083891b",
+    "url": "/deployedQuizApp/static/js/2.97cbba4c.chunk.js"
+  },
+  {
+    "revision": "b013b7cd7a2da92c16a2d9932cfeb76c",
+    "url": "/deployedQuizApp/static/js/2.97cbba4c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebd628652e1d740dd02b",
+    "url": "/deployedQuizApp/static/js/main.ec310e2b.chunk.js"
+  },
+  {
+    "revision": "65da3d37c8be7051d3d3",
+    "url": "/deployedQuizApp/static/js/runtime-main.5b74fd4b.js"
+  }
+]);
